a9e4cd72149f5323c1a15fb3f8f851ae3e2bb839
LeoVinchi454 <122775689+LeoVinchi454@users.noreply.github.com>
2024-03-01 14:34:52 +0200

    Add files via upload

---
+++ C2T18P4/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2T18P4
+{
+    abstract class Person
+    {
+        //позволяющими вывести на экран информацию о персоне,
+        //а также определить ее возраст (на момент текущей даты)
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Fuculty { get; set; }
+        //метод вывода информации
+       
+        public void Output()
+        {
+            Console.WriteLine($"name{Name}\nsurname{Surname}\ndata{DateTime}\n{Fuculty}");
+        }
+    }
+}
